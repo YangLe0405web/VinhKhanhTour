bb11edd52481af51cceedb80b9b19f576d37d190
GiangLe <lehoanggiang0405@gmail.com>
2026-03-22 19:12:31 +0700

    update gemini key

--- VinhKhanhTour.CMS/obj/Debug/net8.0/VinhKhanhTour.CMS.AssemblyInfo.cs
+++ VinhKhanhTour.CMS/obj/Debug/net8.0/VinhKhanhTour.CMS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VinhKhanhTour.CMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e0a9955c49ac948e8d85c6a20d26b1b1c3dfd9e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1abf8be766d586de18e128fb3832c16b6ad486d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("VinhKhanhTour.CMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VinhKhanhTour.CMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
