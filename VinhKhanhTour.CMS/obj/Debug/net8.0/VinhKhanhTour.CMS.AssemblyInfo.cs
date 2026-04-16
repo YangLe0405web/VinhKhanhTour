d33e8fea18cd07c6071e9fe776e121be94643ffb
GiangLe <lehoanggiang0405@gmail.com>
2026-04-16 09:26:23 +0700

    Update: Advanced Analytics Dashboard & POI Hotspots Heatmap

--- VinhKhanhTour.CMS/obj/Debug/net8.0/VinhKhanhTour.CMS.AssemblyInfo.cs
+++ VinhKhanhTour.CMS/obj/Debug/net8.0/VinhKhanhTour.CMS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VinhKhanhTour.CMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad2f88b86b21bd693f4a163dcd849e5bd6905d7f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9856b3ea028c0b8c82d52a6c9ee3543154314dc1")]
 [assembly: System.Reflection.AssemblyProductAttribute("VinhKhanhTour.CMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VinhKhanhTour.CMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
