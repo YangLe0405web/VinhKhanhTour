7b2fef566f0dad60f4e91463c96f3a34436542bf
GiangLe <lehoanggiang0405@gmail.com>
2026-04-16 09:36:11 +0700

    Update: Device Management Tab and History Filtering by DeviceId

--- VinhKhanhTour.CMS/obj/Debug/net8.0/VinhKhanhTour.CMS.AssemblyInfo.cs
+++ VinhKhanhTour.CMS/obj/Debug/net8.0/VinhKhanhTour.CMS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VinhKhanhTour.CMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9856b3ea028c0b8c82d52a6c9ee3543154314dc1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d33e8fea18cd07c6071e9fe776e121be94643ffb")]
 [assembly: System.Reflection.AssemblyProductAttribute("VinhKhanhTour.CMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VinhKhanhTour.CMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
